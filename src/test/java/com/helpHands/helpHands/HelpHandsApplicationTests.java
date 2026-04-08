59747cdd4840ba775770a18945f7a0f72a7a5313
leticia_abraao <leticiaaod@gmail.com>
2026-04-08 18:59:08 -0300

    First commit

---
+++ src/test/java/com/helpHands/helpHands/HelpHandsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helpHands.helpHands;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpHandsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
