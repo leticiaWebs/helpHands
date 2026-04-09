2e7bb4cda8daf6c2c86a4c2121c5b5647359b0a4
leticia_abraao <leticiaaod@gmail.com>
2026-04-09 12:37:31 -0300

    configuração de swagger

---
+++ src/main/java/com/helpHands/helpHands/domain/User.java
@@ -0,0 +1,36 @@
+package com.helpHands.helpHands.domain;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.usertype.UserType;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.OffsetDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String name;
+    private String phone;
+    private MultipartFile photo;
+    private String bio;
+    private UserType  userType;
+    private String rating;
+    private int totalReviews;
+    private String city;
+    private OffsetDateTime createdAt;
+
+
+}
