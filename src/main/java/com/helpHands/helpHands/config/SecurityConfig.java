c2d3389cea80831a2c951806e4bd0294707c92bb
leticia_abraao <leticiaaod@gmail.com>
2026-05-04 22:14:33 -0300

    Chave

--- src/main/java/com/helpHands/helpHands/config/SecurityConfig.java
+++ src/main/java/com/helpHands/helpHands/config/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     @Autowired
+
     private JwtFilter jwtFilter;
 
     @Bean
--- src/main/java/com/helpHands/helpHands/service/JwtService.java
+++ src/main/java/com/helpHands/helpHands/service/JwtService.java
@@ -10,7 +10,7 @@ import java.util.Date;
 @Service
 public class JwtService {
 
-    private final String SECRET = "sua-chave-secreta-super-forte";
+    private final String SECRET = "9f3a7c2b5e4d8a1f6c0b3e9d2a7f4c6e8b1d3f5a9c2e7d4b6f0a8c1e3d5b7f9";
 
     public String gerarToken(String email) {
         return Jwts.builder()
